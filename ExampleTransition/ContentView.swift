834e17ea92350872f5e9d8f084062d9de3f194b0
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2025-06-06 20:39:41 +0900

    Initial Commit

---
+++ ExampleTransition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleTransition
+//
+//  Created by  Guillermo Ignacio Enriquez Gutierrez on 2025/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleTransition/ExampleTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTransitionApp.swift
+//  ExampleTransition
+//
+//  Created by  Guillermo Ignacio Enriquez Gutierrez on 2025/06/06.
+//
+
+import SwiftUI
+
+@main
+struct ExampleTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
